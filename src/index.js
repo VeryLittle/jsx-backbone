e3913ac76497d01a95bb037c4502715d2235ad81
bey <bey@neolant.com>
2021-07-25 17:16:12 +0300

    diff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 // import {buildJsx} from 'estree-util-build-jsx';
 // exports.buildJsx = buildJsx;
 
-var h = require('hyperscript')
-window.hyperscript = h;
+// var h = require('hyperscript')
+// window.hyperscript = h;
+
+// import * as dom from 'incremental-dom';
+// window.dom = dom;
+
+// var h = require('incremental-hyperscript').h;
+// window.hyperscript = h;
+
+import * as diffDom from "diff-dom";
+window.diffDom = diffDom;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development', // production
     entry: './src/index.js',
     output: {
-        filename: 'hyperscript.js',
+        filename: 'diffDom.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
